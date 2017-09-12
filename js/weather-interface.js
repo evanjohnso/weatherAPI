9a6db438adc2eba20f8ebe60c6006f0bcd77050e
Guest User <Guest@Epicodus-6E.local>
2017-09-12 15:59:24 -0700

    fail to promise

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -20,6 +20,23 @@ $(document).ready(function() {
       request.send();
     });
 
+    let promiseTwo = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+
+      let url = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
+
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+
+
     promise.then(function(response) {
       let body = JSON.parse(response);
       let sunriseUnix = body.sys.sunrise;
@@ -28,22 +45,41 @@ $(document).ready(function() {
       let icon = ("<img src='http://openweathermap.org/img/w/" + body.weather[0].icon + ".png'>");
       let sunrise = moment.unix(sunriseUnix).format('hh:mm a');
       let sunset = moment.unix(sunsetUnix).format('hh:mm a');
+      let lat = body.coord.lat;
+      let lon = body.coord.lon;
       $('#showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
       $('#showTemp').text(`The temperature is ${body.main.temp.toFixed(0)} degrees Farenheit.`);
       $('#minTemp').text(`The Low today is ${body.main.temp_min.toFixed(0)} degrees Farenheit.`);
       $('#maxTemp').text(`The High today is ${body.main.temp_max.toFixed(0)} degrees Farenheit.`);
       $('#showSunrise').text(`The Sunrise today in ${city} is ${sunrise}.`);
       $('#showSunset').text(`The Sunset today in ${city} is ${sunset}.`);
-      $('#cityCoords').text(`Your city is ${city} and Longitude/Latitude coordinates are ${body.coord.lon}:${body.coord.lat}`);
+      $('#visibility').text(`Visibility is ${body.visibility}`);
+      $('#cityCoords').text(`Your city is ${city} and Longitude/Latitude coordinates are ${body.coord.lon} : ${body.coord.lat}`);
       $('#weatherIcon').html(icon);
-
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+      console.log(lat);
+      console.log(lon);
+      console.log("this is" + this);
+      let go2 = (lat, lon) => {
+        console.log("Hi I see: " + lat);
+        promiseTwo.then(function(response) {
+          let body = JSON.parse(response);
+          let uvLevel = body.value;
+          $('#uvLevel').text(uvLevel);
+        }, function(error) {
+        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+        });
+      };
+      return go2();
     });
   });
 });
 
-
+    // Promise.all(promise, promiseTwo)
+    // .then(function(results) {
+    //   let body = JSON.parse(response);
+    //   let uvLevel = body.value;
+    //   $('#uvLevel').text(uvLevel);
+    // });
 
 
 
