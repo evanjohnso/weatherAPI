fb8ed8bda6b87e7767c6314e741f81615f8f6b03
Guest User <Guest@Epicodus-6E.local>
2017-09-12 11:51:26 -0700

    update api

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -5,23 +5,60 @@ $(document).ready(function() {
     let city = $('#location').val();
     $('#location').val("");
 
-    $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`).then(function(response) {
-      let sunriseUnix = response.sys.sunrise;
-      let sunsetUnix = response.sys.sunset;
+// start code
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
 
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      let sunriseUnix = body.sys.sunrise;
+      let sunsetUnix = body.sys.sunset;
       let sunrise = moment.unix(sunriseUnix).format('hh:mm a');
       let sunset = moment.unix(sunsetUnix).format('hh:mm a');
-
-      $('#showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('#showTemp').text(`The current temperature is ${response.main.temp.toFixed(0)} degrees Farenheit.`);
+      $('#showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('#showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
       $('#showSunrise').text(`The Sunrise today in ${city} is ${sunrise}.`);
       $('#showSunset').text(`The Sunset today in ${city} is ${sunset}.`);
-    }).fail(function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
 
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
   });
 });
+
+
+
+
+
+
+//     $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`).then(function(response) {
+//       let sunriseUnix = response.sys.sunrise;
+//       let sunsetUnix = response.sys.sunset;
+//
+//       let sunrise = moment.unix(sunriseUnix).format('hh:mm a');
+//       let sunset = moment.unix(sunsetUnix).format('hh:mm a');
+//
+//       $('#showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+//       $('#showTemp').text(`The current temperature is ${response.main.temp.toFixed(0)} degrees Farenheit.`);
+//       $('#showSunrise').text(`The Sunrise today in ${city} is ${sunrise}.`);
+//       $('#showSunset').text(`The Sunset today in ${city} is ${sunset}.`);
+//     }).fail(function(error) {
+//       $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+//
+//     });
+//   });
+// });
 
 // AJAX apiKey
 // $(document).ready(function() {
